6f9a23009a068a6eed436217a2c240bb50c92792
aaron <aaronburbanoms15@gmail.com>
2024-01-03 17:53:08 -0500

    add reference unique the client ,also IntegrityHash for wompi

--- app/Http/Controllers/clienteController.php
+++ app/Http/Controllers/clienteController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Str;
+
 
 use Illuminate\Support\Facades\Validator;
 use App\Models\Cliente;
@@ -57,7 +59,15 @@ class clienteController extends Controller
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 400);
         }
-        
+
+        $reference = Str::uuid()->toString();
+
+        $request['reference'] = $reference;
+        $price = $request['price'];
+
+        $integrityHash = $this->generateIntegrityHash($request);
+
+        $request['hash_integrity'] = $integrityHash;
 
         $cliente = Cliente::create($request->all());
         return response()->json(
@@ -65,15 +75,27 @@ class clienteController extends Controller
                 'status' => 'success',
                 'message' => 'Usuario creado satisfactoriamente',
                 'cliente' => $cliente,
+                'hash_integrity' => $integrityHash,
             ],
             200,
         );
-        
     }
 
+    private function generateIntegrityHash($request)
+    {
+        $referencia = $request['reference'];
+        $monto = $request['price'];
+        $moneda = 'COP';
+        $secretoIntegridad = 'test_integrity_v3iBS12tr8FO83E9VXa1E6cpudVjGdNH';
+        $cadenaConcatenada = "{$referencia}{$monto}{$moneda}{$secretoIntegridad}";
+        // Genera y devuelve el hash de integridad
+        return hash('sha256', $cadenaConcatenada);
+    }
+
+
+    
     public function update(Request $request, $id)
     {
-
         try {
         $rules = [
             "reference" => 'required|string|max:255',
@@ -99,7 +121,16 @@ class clienteController extends Controller
 
     } catch (\Exception $e) {
         return response()->json(['error' => 'cliente no encontrado'], 404);
+        }
     }
-    }
-
 }
+
+
+
+
+
+
+
+
+
+
